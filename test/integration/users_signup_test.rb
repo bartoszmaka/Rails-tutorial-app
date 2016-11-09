b63e6142039a0f04b89809a222617c2409d8f2b6
bartoszmaka <moncznybartosz@gmail.com>
2016-11-09 12:35:44 +0100

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid sighup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: '',
+                                          email: 'user@invalid',
+                                          password: 'foo',
+                                          password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+    assert_select 'form[action="/signup"]'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: 'examplevaliduser',
+                                          email: 'examplevalidemail@valid.com',
+                                          password: '123123',
+                                          password_confirmation: '123123' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    # user = User.find_by email: 'examplevalidemail@valid.com'
+    # user.delete
+  end
+end
